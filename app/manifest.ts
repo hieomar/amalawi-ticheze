dae5ea4adddbdeb04434b69c44a17a677d7511ea
hieomar <iamomar1st@gmail.com>
2025-07-19 16:34:47 +0200

    Add apple-touch-icon.png to public directory

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/next.svg',
+        src: '/android-chrome-192x192.png',
         sizes: '192x192',
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
       {
-        src: '/next.svg',
+        src: '/android-chrome-192x192.png',
         sizes: '512x512', 
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
     ],
   }
